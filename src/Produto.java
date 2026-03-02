54e48e5ce9f7d0391f0773cd0a807f3afe8d8d05
CaioCFalinacio <caio.falinacio@sga.pucminas.br>
2026-03-02 16:15:57 -0300

    feat: adicionada função para conferir se dois produtos são iguais

--- src/Produto.java
+++ src/Produto.java
@@ -67,4 +67,15 @@ public class Produto {
     	
 		return String.format("NOME: " + descricao + ": " + moeda.format(valorDeVenda()));
 	}
+
+    /**
+     * Igualdade de produtos: caso possuam o mesmo nome/descrição.
+     * @param obj Outro produto a ser comparado
+     * @return booleano true/false conforme o parâmetro possua a descrição igual ou não a este produto.
+     */
+    @Override
+    public boolean equals(Object obj){
+        Produto outro = (Produto)obj;
+        return this.descricao.toLowerCase().equals(outro.descricao.toLowerCase());
+    }
 }
